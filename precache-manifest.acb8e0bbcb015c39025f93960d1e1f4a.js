a489cc8777f225806626cf50298ffb945351e1a6
sagar-chandani <spc1194@gmail.com>
2019-03-06 20:50:23 -0600

    Updates

---
+++ precache-manifest.acb8e0bbcb015c39025f93960d1e1f4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jeopardy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f785274d2642212eb88",
+    "url": "/jeopardy/static/js/runtime~main.0f785274.js"
+  },
+  {
+    "revision": "bc8bfab23b92b79f9ab4",
+    "url": "/jeopardy/static/js/main.bc8bfab2.chunk.js"
+  },
+  {
+    "revision": "cc8b8d207e7cafa7efcd",
+    "url": "/jeopardy/static/js/2.cc8b8d20.chunk.js"
+  },
+  {
+    "revision": "bc8bfab23b92b79f9ab4",
+    "url": "/jeopardy/static/css/main.3d44a975.chunk.css"
+  },
+  {
+    "revision": "cc8b8d207e7cafa7efcd",
+    "url": "/jeopardy/static/css/2.e225a65e.chunk.css"
+  },
+  {
+    "revision": "653610fe1f7d926cda6ce991f27d0b56",
+    "url": "/jeopardy/index.html"
+  }
+];
